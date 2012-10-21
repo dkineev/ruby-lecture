e171dc8dd1cb4976517e7a1bf0d35b85db8a544a
dkineev <dkineev84@gmail.com>
2012-10-21 15:25:56 +0400

    first

---
+++ afisha-2012-10-08/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afisha20121008::Application.config.secret_token = '5b9d1deda8478e55e97258075f7786bc423ad9e5d9d7a2aaf5e1693fd2076665f022ae319cd3783b7c4bf54c681542fd1d9a41198332662b8c587ac1535ca9e4'
